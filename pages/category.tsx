12698d05e89052c559e5452ed4d532cc5c464982
Arya <aryanarayantiwari@gmail.com>
2022-03-05 00:14:26 +0530

    update icon size and add hover state for category page

--- pages/category.tsx
+++ pages/category.tsx
@@ -14,67 +14,67 @@ const category = () => {
         />
       </div>
       <ul className="flex flex-col justify-start space-y-4 items-start font-semibold py-2 px-2">
-        <li className="flex w-full justify-between">
+        <li className="flex w-full hover:bg-blue-200 hover:rounded px-1 hover:cursor-pointer justify-between">
             Beauty & Spa
             <img
             src="https://img.icons8.com/external-vitaliy-gorbachev-flat-vitaly-gorbachev/58/000000/external-beauty-hygiene-vitaliy-gorbachev-flat-vitaly-gorbachev.png"
-            className="cursor-pointer w-5 h-5 md:w-16 md:h-16"
+            className="cursor-pointer w-8 h-8"
           />
         </li>
-        <li className="flex w-full justify-between">
+        <li className="flex w-full hover:bg-blue-200 hover:rounded px-1 hover:cursor-pointer justify-between">
             Applicance Repair
             <img
             src="https://img.icons8.com/external-sbts2018-flat-sbts2018/58/000000/external-repair-customer-support-sbts2018-flat-sbts2018.png"
-            className="cursor-pointer w-5 h-5 md:w-16 md:h-16"
+            className="cursor-pointer w-8 h-8"
           />
         </li>
-        <li className="flex w-full justify-between">
+        <li className="flex w-full hover:bg-blue-200 hover:rounded px-1 hover:cursor-pointer justify-between">
             Home Cleaning
             <img
             src="https://img.icons8.com/office/58/000000/broom.png"
-            className="cursor-pointer w-5 h-5 md:w-16 md:h-16"
+            className="cursor-pointer w-8 h-8"
           />
         </li>
-        <li className="flex w-full justify-between">
+        <li className="flex w-full hover:bg-blue-200 hover:rounded px-1 hover:cursor-pointer justify-between">
             Wedding & Events
             <img
             src="https://img.icons8.com/cute-clipart/58/000000/wedding-day.png"
-            className="cursor-pointer w-5 h-5 md:w-16 md:h-16"
+            className="cursor-pointer w-8 h-8"
           />
         </li>
-        <li className="flex w-full justify-between">
+        <li className="flex w-full hover:bg-blue-200 hover:rounded px-1 hover:cursor-pointer justify-between">
             Paints
             <img
             src="https://img.icons8.com/fluency/58/000000/easel.png"
-            className="cursor-pointer w-5 h-5 md:w-16 md:h-16"
+            className="cursor-pointer w-8 h-8"
           />
         </li>
-        <li className="flex w-full justify-between">
+        <li className="flex w-full hover:bg-blue-200 hover:rounded px-1 hover:cursor-pointer justify-between">
             Pest Control
             <img
             src="https://img.icons8.com/flat-round/58/000000/insect--v1.png"
-            className="cursor-pointer w-5 h-5 md:w-16 md:h-16"
+            className="cursor-pointer w-8 h-8"
           />
         </li>
-        <li className="flex w-full justify-between">
+        <li className="flex w-full hover:bg-blue-200 hover:rounded px-1 hover:cursor-pointer justify-between">
             Moving Homes
             <img
             src="https://img.icons8.com/office/58/000000/truck.png"
-            className="cursor-pointer w-5 h-5 md:w-16 md:h-16"
+            className="cursor-pointer w-8 h-8"
           />
         </li>
-        <li className="flex w-full justify-between">
+        <li className="flex w-full hover:bg-blue-200 hover:rounded px-1 hover:cursor-pointer justify-between">
             Plumber
             <img
             src="https://img.icons8.com/external-vitaliy-gorbachev-blue-vitaly-gorbachev/58/000000/external-plumber-labour-day-vitaliy-gorbachev-blue-vitaly-gorbachev.png"
-            className="cursor-pointer w-5 h-5 md:w-16 md:h-16"
+            className="cursor-pointer w-8 h-8"
           />
         </li>
-        <li className="flex w-full justify-between">
+        <li className="flex w-full hover:bg-blue-200 hover:rounded px-1 hover:cursor-pointer justify-between">
             Electrician
             <img
             src="https://img.icons8.com/external-icongeek26-flat-icongeek26/64/000000/external-electrician-electrician-icongeek26-flat-icongeek26.png"
-            className="cursor-pointer w-5 h-5 md:w-16 md:h-16"
+            className="cursor-pointer w-8 h-8"
           />
         </li>
       </ul>
